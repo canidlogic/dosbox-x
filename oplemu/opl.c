b5c955ceee3ba7e93c3a79f5f1018058ec2abbf0
Noah Johnson <noah.johnson@loupmail.com>
2023-01-08 19:05:02 -0600

    Rename to opl.c and edit for C standalone

--- oplemu/opl.c
+++ oplemu/opl.c
@@ -28,10 +28,176 @@
 #include <math.h>
 #include <stdlib.h> // rand()
 #include <string.h> // memset()
-#include "dosbox.h"
 #include "opl.h"
 #include <string.h>
 
+/*
+ * bool definitions so we can compile in C
+ */
+typedef int bool;
+#define true (1)
+#define false (0)
+
+/*
+ * BEGIN Stuff moved in from the header
+ */
+
+/*
+	define attribution that inlines/forces inlining of a function (optional)
+*/
+#define OPL_INLINE INLINE
+
+
+#undef NUM_CHANNELS
+#if defined(OPLTYPE_IS_OPL3)
+#define NUM_CHANNELS	18
+#else
+#define NUM_CHANNELS	9
+#endif
+
+#define MAXOPERATORS	(NUM_CHANNELS*2)
+
+
+#define FL05	((fltype)0.5)
+#define FL2		((fltype)2.0)
+#define PI		((fltype)3.1415926535897932384626433832795)
+
+
+#define FIXEDPT			0x10000		// fixed-point calculations using 16+16
+#define FIXEDPT_LFO		0x1000000	// fixed-point calculations using 8+24
+
+#define WAVEPREC		1024		// waveform precision (10 bits)
+
+#define INTFREQU		((fltype)(14318180.0 / 288.0))		// clocking of the chip
+
+
+#define OF_TYPE_ATT			0
+#define OF_TYPE_DEC			1
+#define OF_TYPE_REL			2
+#define OF_TYPE_SUS			3
+#define OF_TYPE_SUS_NOKEEP	4
+#define OF_TYPE_OFF			5
+
+#define ARC_CONTROL			0x00
+#define ARC_TVS_KSR_MUL		0x20
+#define ARC_KSL_OUTLEV		0x40
+#define ARC_ATTR_DECR		0x60
+#define ARC_SUSL_RELR		0x80
+#define ARC_FREQ_NUM		0xa0
+#define ARC_KON_BNUM		0xb0
+#define ARC_PERC_MODE		0xbd
+#define ARC_FEEDBACK		0xc0
+#define ARC_WAVE_SEL		0xe0
+
+#define ARC_SECONDSET		0x100	// second operator set for OPL3
+
+
+#define OP_ACT_OFF			0x00
+#define OP_ACT_NORMAL		0x01	// regular channel activated (bitmasked)
+#define OP_ACT_PERC			0x02	// percussion channel activated (bitmasked)
+
+#define BLOCKBUF_SIZE		512
+
+
+// vibrato constants
+#define VIBTAB_SIZE			8
+#define VIBFAC				70/50000		// no braces, integer mul/div
+
+// tremolo constants and table
+#define TREMTAB_SIZE		53
+#define TREM_FREQ			((fltype)(3.7))			// tremolo at 3.7hz
+
+
+/* operator struct definition
+     For OPL2 all 9 channels consist of two operators each, carrier and modulator.
+     Channel x has operators x as modulator and operators (9+x) as carrier.
+     For OPL3 all 18 channels consist either of two operators (2op mode) or four
+     operators (4op mode) which is determined through register4 of the second
+     adlib register set.
+     Only the channels 0,1,2 (first set) and 9,10,11 (second set) can act as
+     4op channels. The two additional operators for a channel y come from the
+     2op channel y+3 so the operatorss y, (9+y), y+3, (9+y)+3 make up a 4op
+     channel.
+*/
+typedef struct operator_struct {
+	int32_t cval, lastcval;			// current output/last output (used for feedback)
+	uint32_t tcount, wfpos, tinc;		// time (position in waveform) and time increment
+	fltype amp, step_amp;			// and amplification (envelope)
+	fltype vol;						// volume
+	fltype sustain_level;			// sustain level
+	int32_t mfbi;					// feedback amount
+	fltype a0, a1, a2, a3;			// attack rate function coefficients
+	fltype decaymul, releasemul;	// decay/release rate functions
+	uint32_t op_state;				// current state of operator (attack/decay/sustain/release/off)
+	uint32_t toff;
+	int32_t freq_high;				// highest three bits of the frequency, used for vibrato calculations
+	int16_t* cur_wform;				// start of selected waveform
+	uint32_t cur_wmask;				// mask for selected waveform
+	uint32_t act_state;				// activity state (regular, percussion)
+	bool sus_keep;					// keep sustain level when decay finished
+	bool vibrato,tremolo;			// vibrato/tremolo enable bits
+	
+	// variables used to provide non-continuous envelopes
+	uint32_t generator_pos;			// for non-standard sample rates we need to determine how many samples have passed
+	Bits cur_env_step;				// current (standardized) sample position
+	Bits env_step_a,env_step_d,env_step_r;	// number of std samples of one step (for attack/decay/release mode)
+	uint8_t step_skip_pos_a;			// position of 8-cyclic step skipping (always 2^x to check against mask)
+	Bits env_step_skip_a;			// bitmask that determines if a step is skipped (respective bit is zero then)
+
+#if defined(OPLTYPE_IS_OPL3)
+	bool is_4op,is_4op_attached;	// base of a 4op channel/part of a 4op channel
+	int32_t left_pan,right_pan;		// opl3 stereo panning amount
+#endif
+} op_type;
+
+// per-chip variables
+Bitu chip_num;
+op_type op[MAXOPERATORS];
+
+Bits int_samplerate;
+	
+uint8_t status;
+uint32_t opl_index;
+#if defined(OPLTYPE_IS_OPL3)
+uint8_t adlibreg[512];	// adlib register set (including second set)
+uint8_t wave_sel[44];		// waveform selection
+#else
+uint8_t adlibreg[256];	// adlib register set
+uint8_t wave_sel[22];		// waveform selection
+#endif
+
+
+// vibrato/tremolo increment/counter
+uint32_t vibtab_pos;
+uint32_t vibtab_add;
+uint32_t tremtab_pos;
+uint32_t tremtab_add;
+
+
+// enable an operator
+void enable_operator(Bitu regbase, op_type* op_pt, uint32_t act_type);
+
+// functions to change parameters of an operator
+void change_frequency(Bitu chanbase, Bitu regbase, op_type* op_pt);
+
+void change_attackrate(Bitu regbase, op_type* op_pt);
+void change_decayrate(Bitu regbase, op_type* op_pt);
+void change_releaserate(Bitu regbase, op_type* op_pt);
+void change_sustainlevel(Bitu regbase, op_type* op_pt);
+void change_waveform(Bitu regbase, op_type* op_pt);
+void change_keepsustain(Bitu regbase, op_type* op_pt);
+void change_vibrato(Bitu regbase, op_type* op_pt);
+void change_feedback(Bitu chanbase, op_type* op_pt);
+
+// general functions
+Bitu adlib_reg_read(Bitu port);
+void adlib_write_index(Bitu port, uint8_t val);
+
+static uint32_t generator_add;	// should be a chip parameter
+
+/*
+ * END Stuff moved in from the header
+ */
 
 static fltype recipsamp;	// inverse of sampling rate
 static int16_t wavtable[WAVEPREC*3];	// wave form table
@@ -212,7 +378,7 @@ void operator_output(op_type* op_pt, int32_t modulator, int32_t trem) {
 
 
 // no action, operator is off
-void operator_off(op_type* /*op_pt*/) {
+void operator_off(op_type* op_pt) {
 }
 
 // output level is sustained, mode changes only when operator is turned off (->release)
@@ -535,7 +701,7 @@ void adlib_init(uint32_t samplerate) {
 	for (i=4; i<VIBTAB_SIZE; i++) vib_table[i] = vib_table[i-4]*-1;
 
 	// vibrato at ~6.1 ?? (opl3 docs say 6.1, opl4 docs say 6.0, y8950 docs say 6.4)
-	vibtab_add = static_cast<uint32_t>(VIBTAB_SIZE*FIXEDPT_LFO/8192*INTFREQU/int_samplerate);
+	vibtab_add = (uint32_t)(VIBTAB_SIZE*FIXEDPT_LFO/8192*INTFREQU/int_samplerate);
 	vibtab_pos = 0;
 
 	for (i=0; i<BLOCKBUF_SIZE; i++) vibval_const[i] = 0;
@@ -923,7 +1089,7 @@ void adlib_write_index(Bitu port, uint8_t val) {
 #endif
 }
 
-static void OPL_INLINE clipit16(int32_t ival, int16_t* outval) {
+static void clipit16(int32_t ival, int16_t* outval) {
 	if (ival<32768) {
 		if (ival>-32769) {
 			*outval=(int16_t)ival;
@@ -1462,118 +1628,5 @@ void adlib_getsample(int16_t* sndptr, Bits numsamples) {
 			clipit16(outbufl[i],sndptr++);
 #endif
 
-	}
-}
-
-// save state support
-void adlib_savestate( std::ostream& stream )
-{
-	Bitu cur_wform_idx[MAXOPERATORS];
-
-
-	for( int lcv=0; lcv<MAXOPERATORS; lcv++ ) {
-		cur_wform_idx[lcv] = (Bitu)op[lcv].cur_wform - (Bitu)&wavtable;
-	}
-
-	//****************************************************
-	//****************************************************
-	//****************************************************
-
-	// opl.cpp
-
-	// - pure data
-	WRITE_POD( &recipsamp, recipsamp );
-	WRITE_POD( &wavtable, wavtable );
-
-	WRITE_POD( &vibval_var1, vibval_var1 );
-	WRITE_POD( &vibval_var2, vibval_var2 );
-
-	//****************************************************
-	//****************************************************
-	//****************************************************
-
-	// opl.h
-
-	// - pure data
-	WRITE_POD( &chip_num, chip_num );
-
-	// - near-pure data
-	WRITE_POD( &op, op );
-
-	// - pure data
-	WRITE_POD( &int_samplerate, int_samplerate );
-	WRITE_POD( &status, status );
-	WRITE_POD( &opl_index, opl_index );
-	WRITE_POD( &adlibreg, adlibreg );
-	WRITE_POD( &wave_sel, wave_sel );
-
-	WRITE_POD( &vibtab_pos, vibtab_pos );
-	WRITE_POD( &vibtab_add, vibtab_add );
-	WRITE_POD( &tremtab_pos, tremtab_pos );
-	WRITE_POD( &tremtab_add, tremtab_add );
-	WRITE_POD( &generator_add, generator_add );
-
-
-
-
-	// - reloc ptr (!!!)
-	WRITE_POD( &cur_wform_idx, cur_wform_idx );
-}
-
-
-void adlib_loadstate( std::istream& stream )
-{
-	Bitu cur_wform_idx[MAXOPERATORS];
-
-	//****************************************************
-	//****************************************************
-	//****************************************************
-
-	// opl.cpp
-
-	// - pure data
-	READ_POD( &recipsamp, recipsamp );
-	READ_POD( &wavtable, wavtable );
-
-	READ_POD( &vibval_var1, vibval_var1 );
-	READ_POD( &vibval_var2, vibval_var2 );
-
-	//****************************************************
-	//****************************************************
-	//****************************************************
-
-	// opl.h
-
-	// - pure data
-	READ_POD( &chip_num, chip_num );
-
-	// - near-pure data
-	READ_POD( &op, op );
-
-	// - pure data
-	READ_POD( &int_samplerate, int_samplerate );
-	READ_POD( &status, status );
-	READ_POD( &opl_index, opl_index );
-	READ_POD( &adlibreg, adlibreg );
-	READ_POD( &wave_sel, wave_sel );
-
-	READ_POD( &vibtab_pos, vibtab_pos );
-	READ_POD( &vibtab_add, vibtab_add );
-	READ_POD( &tremtab_pos, tremtab_pos );
-	READ_POD( &tremtab_add, tremtab_add );
-	READ_POD( &generator_add, generator_add );
-
-
-
-
-	// - reloc ptr (!!!)
-	READ_POD( &cur_wform_idx, cur_wform_idx );
-
-	//****************************************************
-	//****************************************************
-	//****************************************************
-
-	for( int lcv=0; lcv<MAXOPERATORS; lcv++ ) {
-		op[lcv].cur_wform = (int16_t *) ((Bitu) &wavtable + cur_wform_idx[lcv]);
 	}
 }
